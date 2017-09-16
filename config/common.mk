1e53e8c210746a3aecfcd9846a40fcb506d971e7
beanstown106 <nbenis106@gmail.com>
2017-09-16 22:41:54 +0530

    add aosp fixs (google props, overlays, phone apks)

--- config/common.mk
+++ config/common.mk
@@ -7,6 +7,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
 		ro.facelock.lively_timeout=2500 \
 		ro.facelock.est_max_time=600 \
 
+include vendor/infinite/config/aosp_fixes.mk
+
 #BootAnimation
 #PRODUCT_COPY_FILES += \
 #		$(LOCAL_PATH)/media/bootanimation.zip:system/media/bootanimation.zip
+
+#Include Overlays
+PRODUCT_PACKAGE_OVERLAYS += \
+		vendor/pure/overlay/common
+
+#Required Packages
+PRODUCT_PACKAGES += \
+		Launcher3 \
+		LiveWallpapersPicker \
+		Stk \
+	 	CellBroadcastReceiver
