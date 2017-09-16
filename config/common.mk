5eeea972aa0c9c7630e5f226dcb245a8374b032c
Sid-Sun <sidharth.soni525@gmail.com>
2017-09-17 00:30:19 +0530

    InfiniteOS versioning [commit 1/n]
    Signed-off-by: Sid-Sun <sidharth.soni525@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -7,13 +7,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 		ro.facelock.lively_timeout=2500 \
 		ro.facelock.est_max_time=600 \
 		ro.build.selinux=1 \
-		net.tethering.noprovisioning=true
+		net.tethering.noprovisioning=true \
+		ro.infinite.version=$(INF_VERSION)
 
 include vendor/infinite/config/aosp_fixes.mk
 
 PRODUCT_COPY_FILES += \
 #		$(LOCAL_PATH)/media/bootanimation.zip:system/media/bootanimation.zip
-		vendor/infinite/prebuilt/bin/sysinit:system/bin/sysinit
+		vendor/infinite/prebuilt/bin/sysinit:system/bin/sysinit \
 		vendor/infinite/prebuilt/etc/init.infinite.rc:root/init.infinite.rc
 
 #Include Overlays
@@ -27,3 +28,18 @@ PRODUCT_PACKAGES += \
 		Stk \
 	 	CellBroadcastReceiver \
 		libprotobuf-cpp-full
+
+export ANDROID_VERSION=8.0.0
+
+ifeq ($(KBUILD_BUILD_USER),PlaceHolder)
+		ifeq ($(KBUILD_BUILD_HOST),Europa)
+				INF_BUILD_TYPE := OFFICIAL
+		endif
+endif
+
+#InfiniteOS system versioning
+ifndef INF_BUILD_TYPE
+		INF_BUILD_TYPE := UNOFFICIAL
+endif
+
+INF_VERSION := InfiniteOS_$(PRODUCT_MODEL)_$(ANDROID_VERSION)_$(shell date +%m%d%Y)_$(INF_BUILD_TYPE)
